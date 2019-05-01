4d2b9d74a0c26eb212f72ead93554ff805d25db7
AdamMorris377 <49308206+AdamMorris377@users.noreply.github.com>
2019-05-01 12:57:56 +0100

    Create levels.js

---
+++ levels/levels.js
@@ -0,0 +1,43 @@
+var level1 = { 
+  "grid": [ "...",
+            "#..",
+            "#.."],
+  "grid_width": 3,
+  "grid_height": 3,
+  "actors": [
+    {
+      "type": "Normal Mouse",
+      "x": 0, 
+      "y": 0
+    },
+    {
+      "type": "Cheese",
+      "x": 1,
+      "y": 1
+    }
+  ],
+  "items": ["House"]
+};
+
+var level2 = {
+  "grid": [ "...",
+            "..#",
+            "..."],
+  "grid_width": 3,
+  "grid_height": 3,
+  "actors": [
+    {
+      "type": "Normal Mouse",
+      "x": 0, 
+      "y": 0
+    },
+    {
+      "type": "House",
+      "x": 2,
+      "y": 2
+    }
+  ],
+  "items": ["Cheese"]
+}
+
+var levels = [level1, level2];
