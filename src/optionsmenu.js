5a3122008dc2d37bd5140e4465d147239b3d3305
AdamMorris377 <49308206+AdamMorris377@users.noreply.github.com>
2019-05-09 15:13:11 +0100

    add Restart button and improve design

--- src/optionsmenu.js
+++ src/optionsmenu.js
@@ -17,54 +17,58 @@ class OptionsMenu extends Phaser.Scene {
         const option1_y = 160;
         const option2_y = 225;
         const option3_y = 290;
-        const button_colour = 0x8B4513;
+        const option4_y = 355;
+        const button_image = 'panel';
+        //const button_colour = 0x8B4513;
 
         this.add.rectangle(width/2, height/2, width, height, 0, 0.8);
         
-        var x = this.add.text(160, 320, "nothing pressed"); // for testing
+        var x = this.add.text(160, 500, "nothing pressed"); // for testing
         
         // "Options Menu" text box
         this.add.text(100, 100, 'Options Menu');
         
-        // Button to resume the level
-        var p1 = this.add.image(0, 0, 'panel').setScale(0.2,0.05);
-        //var r1 = this.add.rectangle(0, 0, 125, 50, 0x0000ff); // blue rectangle as alternative to panel background
-        var t1 = this.add.text(10, 0, "Resume");
+        // Button to resume current level
+        var p1 = this.add.image(option_x, option1_y, button_image).setScale(0.2,0.05)
+                     .setInteractive()
+                     .on('pointerdown', function (ev1) {
+                         //x.text = "resume pressed";
+                         $this.scene.resume('displayModel');
+                         $this.scene.stop();
+                     });
+        var t1 = this.add.text(option_x, option1_y, "Resume");
         t1.setOrigin(0.5);
-        var button1 = this.add.container(option_x, option1_y, [p1, t1]);
-        var c1 = this.add.circle(option_x-60, option1_y, 15, button_colour);
-        c1.setInteractive();
-        c1.on("pointerdown", function (ev) {
-                //x.text = "resume pressed";
-                $this.scene.resume('displayModel');
-                $this.scene.stop();
-            })
+            
+        // Button to go to restart current level
+        var p2 = this.add.image(option_x, option2_y, button_image).setScale(0.2,0.05)
+                     .setInteractive()
+                     .on('pointerdown', function (ev2) {
+                         //x.text = "restart pressed";
+                         $this.scene.restart('displayModel');
+                         $this.scene.stop();
+                     });
+        var t2 = this.add.text(option_x, option2_y, "Restart");
+        t2.setOrigin(0.5);
         
         // Button to go to help page
-        var p2 = this.add.image(0, 0, 'panel').setScale(0.2,0.05);
-        var t2 = this.add.text(10, 0, "Help");
-        t2.setOrigin(0.5);
-        var button2 = this.add.container(option_x, option2_y, [p2, t2]);
-        
-        var c2 = this.add.circle(option_x-60, option2_y, 15, button_colour);
-        c2.setInteractive();
-        c2.on("pointerdown", function (ev) {
-                x.text = "help pressed";
-            });
+        var p3 = this.add.image(option_x, option3_y, button_image).setScale(0.2,0.05)
+                     .setInteractive()
+                     .on('pointerdown', function (ev3) {
+                         x.text = "help pressed";
+                     });
+        var t3 = this.add.text(option_x, option3_y, "Help");
+        t3.setOrigin(0.5);
         
         // Button to quit to main menu
-        var p3 = this.add.image(0, 0, 'panel').setScale(0.2,0.05);
-        var t3 = this.add.text(10, 0, "Quit");
-        t3.setOrigin(0.5);
-        var button3 = this.add.container(option_x, option3_y, [p3, t3]);
-        
-        var c3 = this.add.circle(option_x-60, option3_y, 15, button_colour);
-        c3.setInteractive();
-        c3.on("pointerdown", function (ev) {
-                //x.text = "quit pressed";
-                $this.scene.start('levelSelect');
-                $this.scene.stop('displayModel');
-            });
+        var p4 = this.add.image(option_x, option4_y, button_image).setScale(0.2,0.05)
+                     .setInteractive()
+                     .on('pointerdown', function (ev4) {
+                         //x.text = "quit pressed";
+                         $this.scene.start('levelSelect');
+                         $this.scene.stop('displayModel');
+                     });
+        var t4 = this.add.text(option_x, option4_y, "Quit");
+        t4.setOrigin(0.5);
     }
 }
 /*var config = {
