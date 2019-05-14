f19927f5ebc54c2f99952160ad95461e0bcb7206
TeodoraMusatoiu <44507837+TeodoraMusatoiu@users.noreply.github.com>
2019-05-14 12:09:45 +0100

    Added next level button

--- src/emailpage.js
+++ src/emailpage.js
@@ -6,7 +6,7 @@ class EmailPage extends Phaser.Scene {
         preload() {
             this.load.image('panel', 'assets/img/panel.jpg');
             this.load.image('mouse', 'assets/img/mouse.png');
-            this.load.image('button', 'assets/img/levelSelect.png');
+
         }
 
         create() {
@@ -16,18 +16,59 @@ class EmailPage extends Phaser.Scene {
             var y = height / 2000;
             if (y < x) x = y
 
+            const button_x = width / 2;
+            const button1_y = 300;
+            const button2_y = 350;
+            const button_image = 'panel';
+
             this.add.image(width / 2, height / 2, 'panel');
-            this.add.text(width / 2, 200, 'Well done for finishing some of the Mouse Story!', { fontFamily: 'Arial' }).setFontSize(38).setOrigin(0.5);
+            this.add.text(width / 2, 170, 'Well done for finishing some of the Mouse Story!', { fontFamily: 'Arial' }).setFontSize(38).setOrigin(0.5);
             this.add.text(width / 2, 400, 'Enter your email in the box if you would like ', { fontFamily: 'Arial' }).setFontSize(28).setOrigin(0.5);
             this.add.text(width / 2, 450, '    to hear more from Metaswitch ', { fontFamily: 'Arial' }).setFontSize(28).setOrigin(0.5);
             var mouse = this.add.sprite(width / 2, 600, 'mouse').setScale(x, x);
-            this.add.text(width / 2, 270, 'Press Level Select if you want to keep playing!', { fontFamily: 'Arial' }).setFontSize(28).setOrigin(0.5);
-            var button = this.add.sprite(width / 2, 350, 'button').setScale(x * 2.5, x * 2.5);
-            button.setInteractive()
-            button.on('pointerdown', function(ev) {
-                //x.text = "restart pressed";
-                $this.scene.start('levelSelect');
-            });
+            this.add.text(width / 2, 250, 'Press one of the buttons if you want to keep playing!', { fontFamily: 'Arial' }).setFontSize(28).setOrigin(0.5);
+
+            function nextLevel() {
+                var thisLevel = parseInt(model.name.match(/\d+/g));
+                console.log(thisLevel);
+                var next = thisLevel + 1;
+                console.log(next);
+                var levelCount = Object.keys(levels).filter(function(key) {
+                    return key.indexOf('level') === 0;
+                }).length;
+                console.log(levelCount);
+                if (next <= levelCount) {
+                    return ('level' + next);
+                } else {
+                    return '0';
+                }
+            }
+
+            var p1 = this.add.image(button_x, button1_y, button_image).setScale(0.2, 0.05)
+                .setInteractive()
+                .on('pointerdown', function(ev1) {
+                    var next = nextLevel();
+                    if (next === '0') {
+                        $this.scene.start('levelSelect');
+                        $this.scene.stop();
+                    } else {
+                        model.readLevelFromFile(next);
+                        $this.scene.start('displayModel');
+                        $this.scene.stop();
+                    }
+                });
+            var t1 = this.add.text(button_x, button1_y, "Next level");
+            t1.setOrigin(0.5);
+
+            var p2 = this.add.image(button_x, button2_y, button_image).setScale(0.2, 0.05)
+                .setInteractive()
+                .on('pointerdown', function(ev2) {
+                    $this.scene.start('levelSelect');
+                    $this.scene.stop('displayModel');
+                    $this.scene.stop();
+                });
+            var t2 = this.add.text(button_x, button2_y, "Level Select");
+            t2.setOrigin(0.5);
 
             setTimeout(function() {
                 var email = window.prompt("Please enter your email", "Email Address");
